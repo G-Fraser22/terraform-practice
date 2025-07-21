e7aac19008b7fdefe0bb8a8c404fc452f614ad1a
G-Fraser22 <frasergina@live.com>
2025-07-21 15:40:33 +0200

    firstcommi

--- practice-s3.tf
+++ practice-s3.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_s3_bucket" "my_bucket" {
-  bucket = "my-unique-bucket-name-ginafraser1"  # Must be globally unique
+  bucket = "my-unique-bucket-name-gin-fras"  # Must be globally unique
 
   tags = {
     Name        = "MyS3Bucket"
